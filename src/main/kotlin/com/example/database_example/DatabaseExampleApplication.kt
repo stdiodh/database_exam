2cba81a976134d96717cbe1c3f23400e0b04f617
stdiodh <playlistdh@gmail.com>
2024-05-08 23:06:11 +0900

    h2_Database

---
+++ src/main/kotlin/com/example/database_example/DatabaseExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.database_example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DatabaseExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<DatabaseExampleApplication>(*args)
+}
